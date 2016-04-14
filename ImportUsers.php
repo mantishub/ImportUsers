2ce2c79a36fcc275e88f03bdbbc1f5192838c635
Victor Boctor <victor@mantishub.net>
2016-04-13 21:52:53 -0700

    Use plugin page instead of adding a menu option

--- ImportUsers.php
+++ ImportUsers.php
@@ -4,22 +4,11 @@ class ImportUsersPlugin extends MantisPlugin
 	function register() {
 		$this->name = plugin_lang_get( 'title' );
 		$this->description = plugin_lang_get( 'description' );
+		$this->page = 'import_users_page_init';
 		$this->version = '1.0';
 		$this->requires = array( 'MantisCore' => '1.3.0' );
 		$this->author = 'MantisHub';
 		$this->contact = 'http://www.mantishub.com';
 		$this->url = 'http://www.mantishub.com';
-	}
-
-	function hooks() {
-		return array(
-			'EVENT_MENU_MANAGE' => 'csv_import_menu',
-		);
-	}
-
-	function csv_import_menu() {
-		return array(
-			'<a href="' . plugin_page( 'import_users_page_init' ) . '">' . plugin_lang_get( 'import_users' ) . '</a>',
-		);
 	}
 }
